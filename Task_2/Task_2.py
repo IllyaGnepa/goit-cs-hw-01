b112bc509a0dcd35438e9e83ec427a09f9669777
Illia Medvid-Hnepa <illyagnepa1@gmail.com>
2025-01-25 20:48:02 +0200

    Corrected homework

--- Task_2/Task_2.py
+++ Task_2/Task_2.py
@@ -1,6 +1,6 @@
 import re
 
-class TokenType:
+class Lexer:
     PLUS = 'PLUS'
     MINUS = 'MINUS'
     MUL = 'MUL'
@@ -10,29 +10,31 @@ class TokenType:
     RPAREN = 'RPAREN'
     EOF = 'EOF'
 
-class Token:
-    def __init__(self, type_, value=None):
-        self.type = type_
-        self.value = value
+    class Token:
+        def __init__(self, type_, value=None):
+            self.type = type_
+            self.value = value
 
-    def __repr__(self):
-        return f"Token({self.type}, {repr(self.value)})"
+        def __repr__(self):
+            return f"Token({self.type}, {repr(self.value)})"
 
-class Lexer:
     def __init__(self, text):
         self.text = text
         self.pos = 0
         self.current_char = self.text[self.pos]
 
+    def error(self):
+        raise Exception('Invalid character')
+
     def advance(self):
         self.pos += 1
         if self.pos > len(self.text) - 1:
-            self.current_char = None
+            self.current_char = None 
         else:
             self.current_char = self.text[self.pos]
 
     def skip_whitespace(self):
-        while self.current_char is not None and self.current_char.isspace():
+        while self.current_char is not None and self.current_char in ' \t':
             self.advance()
 
     def integer(self):
@@ -44,145 +46,99 @@ class Lexer:
 
     def get_next_token(self):
         while self.current_char is not None:
-            if self.current_char.isspace():
+            if self.current_char in ' \t':
                 self.skip_whitespace()
                 continue
+
+            if self.current_char.isdigit():
+                return self.Token(self.INT, self.integer())
+
             if self.current_char == '+':
                 self.advance()
-                return Token(TokenType.PLUS)
+                return self.Token(self.PLUS)
+
             if self.current_char == '-':
                 self.advance()
-                return Token(TokenType.MINUS)
+                return self.Token(self.MINUS)
+
             if self.current_char == '*':
                 self.advance()
-                return Token(TokenType.MUL)
+                return self.Token(self.MUL)
+
             if self.current_char == '/':
                 self.advance()
-                return Token(TokenType.DIV)
+                return self.Token(self.DIV)
+
             if self.current_char == '(':
                 self.advance()
-                return Token(TokenType.LPAREN)
+                return self.Token(self.LPAREN)
+
             if self.current_char == ')':
                 self.advance()
-                return Token(TokenType.RPAREN)
-            if self.current_char.isdigit():
-                return Token(TokenType.INT, self.integer())
-            raise Exception('Невідомий символ')
+                return self.Token(self.RPAREN)
 
-        return Token(TokenType.EOF)
+            self.error()
 
-class AST:
-    pass
+        return self.Token(self.EOF)
 
-class BinOp(AST):
-    def __init__(self, left, op, right):
-        self.left = left
-        self.op = op
-        self.right = right
-
-class Num(AST):
-    def __init__(self, value):
-        self.value = value
-
-class UnaryOp(AST):
-    def __init__(self, op, expr):
-        self.op = op
-        self.expr = expr
-
-class Paren(AST):
-    def __init__(self, expr):
-        self.expr = expr
 
 class Parser:
     def __init__(self, lexer):
         self.lexer = lexer
         self.current_token = self.lexer.get_next_token()
 
+    def error(self):
+        raise Exception('Invalid syntax')
+
     def eat(self, token_type):
         if self.current_token.type == token_type:
             self.current_token = self.lexer.get_next_token()
         else:
-            raise Exception(f'Помилка синтаксису: очікується {token_type}')
+            self.error()
 
     def factor(self):
         token = self.current_token
-        if token.type == TokenType.INT:
-            self.eat(TokenType.INT)
-            return Num(token.value)
-        elif token.type == TokenType.LPAREN:
-            self.eat(TokenType.LPAREN)
-            node = self.expr()
-            self.eat(TokenType.RPAREN)
-            return Paren(node)
-        elif token.type == TokenType.PLUS:
-            self.eat(TokenType.PLUS)
-            return UnaryOp(token, self.factor())
-        elif token.type == TokenType.MINUS:
-            self.eat(TokenType.MINUS)
-            return UnaryOp(token, self.factor())
+        if token.type == Lexer.INT:
+            self.eat(Lexer.INT)
+            return token.value
+        elif token.type == Lexer.LPAREN:
+            self.eat(Lexer.LPAREN)
+            result = self.expr()
+            self.eat(Lexer.RPAREN)
+            return result
 
     def term(self):
-        node = self.factor()
-
-        while self.current_token.type in (TokenType.MUL, TokenType.DIV):
+        result = self.factor()
+        while self.current_token.type in (Lexer.MUL, Lexer.DIV):
             token = self.current_token
-            if token.type == TokenType.MUL:
-                self.eat(TokenType.MUL)
-            elif token.type == TokenType.DIV:
-                self.eat(TokenType.DIV)
-            node = BinOp(left=node, op=token, right=self.factor())
-
-        return node
+            if token.type == Lexer.MUL:
+                self.eat(Lexer.MUL)
+                result *= self.factor()
+            elif token.type == Lexer.DIV:
+                self.eat(Lexer.DIV)
+                result /= self.factor()
+        return result
 
     def expr(self):
-        node = self.term()
-
-        while self.current_token.type in (TokenType.PLUS, TokenType.MINUS):
+        result = self.term()
+        while self.current_token.type in (Lexer.PLUS, Lexer.MINUS):
             token = self.current_token
-            if token.type == TokenType.PLUS:
-                self.eat(TokenType.PLUS)
-            elif token.type == TokenType.MINUS:
-                self.eat(TokenType.MINUS)
-            node = BinOp(left=node, op=token, right=self.term())
+            if token.type == Lexer.PLUS:
+                self.eat(Lexer.PLUS)
+                result += self.term()
+            elif token.type == Lexer.MINUS:
+                self.eat(Lexer.MINUS)
+                result -= self.term()
+        return result
 
-        return node
 
 class Interpreter:
     def __init__(self, parser):
         self.parser = parser
 
     def interpret(self):
-        return self.visit(self.parser.expr())
+        return self.parser.expr()
 
-    def visit(self, node):
-        method_name = f"visit_{type(node).__name__}"
-        method = getattr(self, method_name)
-        return method(node)
-
-    def visit_BinOp(self, node):
-        if node.op.type == TokenType.PLUS:
-            return self.visit(node.left) + self.visit(node.right)
-        elif node.op.type == TokenType.MINUS:
-            return self.visit(node.left) - self.visit(node.right)
-        elif node.op.type == TokenType.MUL:
-            return self.visit(node.left) * self.visit(node.right)
-        elif node.op.type == TokenType.DIV:
-            right_value = self.visit(node.right)
-            if right_value == 0:
-                raise ZeroDivisionError("Ділення на нуль!")
-            return self.visit(node.left) / right_value
-
-    def visit_Num(self, node):
-        return node.value
-
-    def visit_UnaryOp(self, node):
-        if node.op.type == TokenType.PLUS:
-            return +self.visit(node.expr)
-        elif node.op.type == TokenType.MINUS:
-            return -self.visit(node.expr)
-
-    def visit_Paren(self, node):
-        return self.visit(node.expr)
 
 # Тестові випадки
 test_cases = [
